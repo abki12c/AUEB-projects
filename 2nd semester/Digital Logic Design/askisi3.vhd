367149f4de7abfbe09fabca583a0c545ce5753eb
Komnas Kafasis <58307243+abki12c@users.noreply.github.com>
2022-04-27 02:30:28 +0300

    Delete askisi3.vhd

--- 2nd semester/Digital Logic Design/askisi3.vhd
+++
@@ -1,9 +0,0 @@
-ENTITY askisi3 IS
-    PORT (x1,x2,x3  : IN  BIT;
-          f         : OUT BIT);
-END askisi3;
-
-ARCHITECTURE Behavior OF askisi3 IS
-BEGIN
-   f <= x2 OR (NOT x1 AND x3);
-END Behavior;
